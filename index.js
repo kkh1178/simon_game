17567e2e13eb3b66f480b7327e3e89bb687ba63d
khill <kalisha.hill@gmail.com>
2020-08-06 17:29:53 -0500

    Updating the index.js to include functions to play sound and add or remove classes

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 var gamePattern = [];
 var userClickedPattern = [];
 buttonColors = ["red", "blue", "green", "yellow"];
+let level = 0;
 
+// Choose a random whole number between 0 and 3
 function nextSequence () {
     var randomNumber = Math.floor(Math.random()*4);
     console.log(randomNumber);
     return randomNumber;
+    level++;
+    // $("h1").text(`Level ${level}`)
 };
 
+// Chose the random color and push to the game pattern array
 var randomChosenColor = buttonColors[nextSequence()];
 console.log(randomChosenColor);
 gamePattern.push(randomChosenColor);
 
 
-
+// When the randomChosenColor is clicked play a sound and "blink"
 $("#"+randomChosenColor).click(function(e) {
     const sound = new Audio("./sounds/"+randomChosenColor+".mp3")
     sound.play();
@@ -21,7 +26,8 @@ $("#"+randomChosenColor).click(function(e) {
 
     });
 
-
+// Listen for a button click and then get the user chosen color; play the 
+// sound and push the color to the user selected array
 $('.btn').on('click', function() {
     
     // console.log($(this)[0].id);
@@ -33,14 +39,19 @@ $('.btn').on('click', function() {
     console.log("user Clicked this", userClickedPattern)
 })
 
-function playSound(name) {
-    $(name).playAudio();
-}
-
+// add a class of pressed to the color we selected and then remove pressed after a delay
 function animatePress(currentColor) {
-    $(currentColor).addClass("pressed");
+    $("#"+currentColor).addClass("pressed");
 
     setTimeout(function() {
-        $(currentColor).removeClass("pressed");
+        $("#"+currentColor).removeClass("pressed");
     }, 100);
 }
+
+$(document).keydown(function() {
+    nextSequence();
+    $("h1").text("Level 0")
+});
+
+
+
